c2d3c299ec81c16cd2c177e6918371f7b01032c4
Aditi Lahare <aditilahare@Aditi-Lahare.local>
2019-02-22 15:14:35 +0530

    Finished the move-grid-left function.

--- src/twenty48/core.clj
+++ src/twenty48/core.clj
@@ -1,22 +1,49 @@
 (ns twenty48.core
   (:gen-class))
 
-(defn move-grid-right
-  "Moves an entire grid to the right"
-  [grid]
-  grid)
+; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(def get-rid-of-zeros (partial remove zero?))
+
+(def group-by-2-consecutive-equals
+  (comp (partial mapcat (partial partition-all 2)) (comp (partial partition-by identity) (partial get-rid-of-zeros))))
+
+(def add-consecutive-equals
+  (comp (partial map (partial apply +)) group-by-2-consecutive-equals))
+
+(defn add-subsequent-zeros [row]
+  (take 4 (concat row (repeat 0))))
+
+(def move-left (comp add-subsequent-zeros add-consecutive-equals))
 
 (defn move-grid-left
   "Moves an entire grid to the left"
   [grid]
-  grid)
+  (map move-left grid)
+  )
+
+; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+(defn move-grid-right
+  "Moves an entire grid to the right"
+  [grid]
+  grid
+  )
+
+; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
 
 (defn move-grid-down
   "Moves an entire grid down"
   [grid]
   grid)
 
+; >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+
 (defn move-grid-up
   "Moves an entire grid up"
   [grid]
   grid)
+
+
